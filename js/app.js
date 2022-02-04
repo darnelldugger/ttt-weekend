d508ca6a729c98f1fd938f3e7e1418d10f9d1246
darnelld <darnelldugger@gmail.com>
2022-02-04 13:16:33 -0500

    finish 3.2

--- js/app.js
+++ js/app.js
@@ -1,14 +1,24 @@
 /*-------------------------------- Constants --------------------------------*/
-console.log('test')
+
 
 
 /*---------------------------- Variables (state) ----------------------------*/
+let arrSquares = []
+let yourTurn, winner, aTie
 
 
 
 /*------------------------ Cached Element References ------------------------*/
-
-
+const row11 = document.querySelector('#sq0')
+const row12 = document.querySelector('#sq1')
+const row13 = document.querySelector('#sq2')
+const row21 = document.querySelector('#sq3')
+const row22 = document.querySelector('#sq4')
+const row23 = document.querySelector('#sq5')
+const row31 = document.querySelector('#sq6')
+const row32 = document.querySelector('#sq7')
+const row33 = document.querySelector('#sq8')
+const gameMsg = document.querySelector('#message')
 
 /*----------------------------- Event Listeners -----------------------------*/
 
@@ -16,3 +26,16 @@ console.log('test')
 
 /*-------------------------------- Functions --------------------------------*/
 
+init()
+function init() {
+  row11.map() = ""
+  row12.map() = ""
+  row13.map() = ""
+  row21.map() = ""
+  row22.map() = ""
+  row23.map() = ""
+  row31.map() = ""
+  row32.map() = ""
+  row33.map() = ""
+
+}
